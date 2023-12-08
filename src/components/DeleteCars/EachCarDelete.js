49570613bd734376823b23e6121f586bbb959cba
Tanmayi Manku <tanmayitanum@gmail.com>
2023-12-08 21:47:36 +0530

    Implement delete car feature(changes the badge)

---
+++ src/components/DeleteCars/EachCarDelete.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { useDispatch } from "react-redux";
+import { deleteCar } from "../../redux/thunk";
+import { markCarDelete } from "../../redux/cars/carsSlice";
+
+const EachCarDelete = ({car}) => {
+    const {
+        name,
+        id,
+        removed
+    } = car;
+
+    const dispatch = useDispatch();
+    const handleDelete = () => {
+        dispatch(deleteCar(id));
+        if(!removed) {
+            dispatch(markCarDelete(id))
+        }
+    }
+
+    return (
+        <tr>
+            <td>{name}</td>
+            <td><button type='button' className={removed ? 'removed-mark' : 'remove-mark'} onClick={handleDelete}>{removed ? 'removed' : 'DELETE'}</button></td>
+        </tr>
+    )
+}
+
+EachCarDelete.propTypes = {
+    car: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
+        removed: PropTypes.bool.isRequired,
+    }).isRequired,
+};
+
+export default EachCarDelete;
--- src/redux/cars/carsSlice.js
+++ src/redux/cars/carsSlice.js
@@ -1,1 +1,56 @@
-// carsSlice File.
+import { createSlice } from '@reduxjs/toolkit';
+import { deleteCar, fetchCars } from '../thunk';
+
+const initialState = {
+    cars: [],
+    status: 'idle',
+    error: null
+};
+
+
+const carSlice = createSlice({
+    name: 'cars',
+    initialState,
+    reducers: {
+        markCarDelete: (state, action) => {
+            const newState = state.cars.map((car) => (car.carId === action.payload ? ({...car, removed: true}) : car));
+            state.cars = newState;
+        }
+    },
+    extraReducers: (builder) => {
+      builder
+        .addCase(fetchCars.pending, (state) => {
+            state.status = 'loading';
+            state.error = null;
+        })
+        .addCase(fetchCars.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            const fetchedData = [];
+            action.payload.forEach((car) => {
+                const data = {...car, removed: false}
+                fetchedData.push(data);
+            })
+            state.cars = fetchedData;
+            console.log('the cars: ')
+            console.log(state.cars);
+        })
+        .addCase(fetchCars.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message;
+        })
+        .addCase(deleteCar.pending, (state) => {
+          state.status = 'loading';
+          state.error = null;
+        })
+        .addCase(deleteCar.fulfilled, (state, action) => {
+          state.status = 'succeeded';
+          state.cars = state.cars.filter((car) => car.carId !== action.payload.id);        })
+        .addCase(deleteCar.rejected, (state, action) => {
+          state.status = 'failed';
+          state.error = action.payload ? action.payload.error : 'Delete failed';
+        });
+    },
+  });
+  
+  export const {markCarDelete} = carSlice.actions;
+  export default carSlice.reducer;
